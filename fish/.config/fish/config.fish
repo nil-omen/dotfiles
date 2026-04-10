6e0f7d65aa60e654d77c8e82c0baa672697ca61c
King <24613526+nil-omen@users.noreply.github.com>
2026-04-10 13:56:27 +0200

    added mise support

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -128,6 +128,7 @@ if status is-interactive
     if type -q zoxide; zoxide init fish | source; end
     if type -q starship; starship init fish | source; end
     if type -q direnv; direnv hook fish | source; end
+    if type -q mise; mise activate fish | source; end
 
     # Set editor and visual
     set -gx EDITOR hx
